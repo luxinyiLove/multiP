7ca3f7cf1a241d25bd46decf9ddfeb52fb77059b
why <1522213037.qq.com>
2018-10-17 08:54:46 +0800

    why&jht7finished

--- src/scripts/controllers/location.js
+++ src/scripts/controllers/location.js
@@ -1,11 +1,7 @@
-// import locationTpl from "../views/location.html";
 import addressModel from "../models/location";
 import locationTpl from "../views/location-city.html";
 
 const render = async ()=>{
-    // $(".home-container").css("display","none");
-    // $(".locate").html(locationTpl).css("display","flex");
-    // console.log($(".home-container"));
     $(".back-main").on("tap",function(){
         location.hash = "#main";
     })
